960f1580915a8882bb9c6d5eb2047eb7b433bc2f
coool8192-arch <coool8192@gmail.com>
2025-12-16 21:43:48 +0900

    251216 실수를 표현하는 방법

--- Program/Source.c
+++ Program/Source.c
@@ -2,45 +2,93 @@
 
 void main()
 {
-#pragma region 오버플로우
-	// 특정한 자료형이 표현할 수 있는 최댓값의 범위를
-	// 넘어서 연산을 수행하는 과정입니다.
+#pragma region 조건문
+	// 어떤 조건이 주어질 때 해당 조건에 따라 동작을
+	// 수행하도록 실행하는 명령문입니다.
 
-	//char storage = 127 + 1;	// 0111 1111
-								// 0000 1111
-								//----------
-								// 1000 0000
+#pragma region 관계 연산자
+	// 두 개의 피연산자의 값을 비교하여 그 결과를 0 또는
+	// 1이라는 값으로 나타내는 연산자입니다.
 
-	//printf("storage :%d\n", storage);
+	int accuracy = 10;
+	int attack = 20;
 
-	// 오버플로우는 부호 없는 자료형에서도 똑같이 발생하며,
-	// 실수일 때 오버플로우가 발생하면 infinity라는 값으로 출력됩니다.
+	//printf("accuracy < attack : %d\n", accuracy < attack);
 
-#pragma endregion
+	//printf("accuracy > attack : %d\n", accuracy > attack);
 
-#pragma region 언더플로우
-	// 특정한 자료형이 표현할 수 있는 최솟값의 범위를
-	// 넘어서 연산을 수행하는 과정입니다.
+	//printf("accuracy <= attack : %d\n", accuracy <= attack);
 
-	//char data = -128 - 1;	// 1000 0000
-							// 1111 1111
-							//----------
-							// 0111 1111
+	//printf("accuracy >= attack : %d\n", accuracy >= attack);
 
-	//printf("data : %d\n", data);
+	//printf("accuracy != attack : %d\n", accuracy != attack);
 
-	// 언더플로우는 부호 없는 자료형에서도 똑같이 발생하며,
-	// 실수일 때 언더플로우가 발생하면 0이라는 값으로 출력됩니다.
+	//printf("accuracy == attack : %d\n", accuracy == attack);
+
+	//printf("30 < 50 : %d\n", 30 < 50);
+
+	// 관계 연산자는 조건이 맞을 때 1이라는 값으로 반환되며,
+	// 조건이 틀릴 때 0이라는 값으로 반환됩니다.
 
 #pragma endregion
 
-#pragma region 부호 없는 자료형
+#pragma region if문
+	// 어떤 특정한 조건을 비교하여 조건이 맞다면 실행되는
+	// 명령문입니다.
 
-	//unsigned int count = 4294967295;
+	//int health = 0;
 
-	//printf("signed count 변수의 값 :%d\n", count);
+	//if(health <= 0)
+	//{
+	//	printf("destroyed");
+	//}
 
-	//printf("unsigned count 변수의 값 : %u\n", count);
+	// if문은 조건이 1일 때 실행되며, 0일 때 실행되지 않습니다.
+#pragma endregion
+
+#pragma region else if문
+	// if문의 조건이 틀릴 때 else if문의 조건이
+	// 맞다면 실행되는 명령문입니다.
+
+	//int level = 30;
+	//
+	//if (level < 30)
+	//{
+	//	printf("You can't enter here.");
+	//}
+	//else if (level >= 30)
+	//{
+	//	printf("Welcome to dungeon.");
+	//}
+	//
+	// else if문은 여러번 정의할 수 있으며, if문이
+	// 존재할 때 사용할 수 있습니다.
+
+#pragma endregion
+
+#pragma region else문
+	// if문과 else if문의 조건이 다 틀리면
+	// 실행되는 명령문입니다.
+
+	int integer = 0;
+
+	if (integer > 0)
+	{
+		printf("positive");
+	}
+	else if (integer < 0)
+	{
+		printf("negative");
+	}
+	else
+	{
+		printf("zero");
+	}
+	
+	// if문에 연결된 모든 조건문의 조건이 맞을 때
+	// 가장 위에 있는 조건문만 실행됩니다.
+
+#pragma endregion
 
 #pragma endregion
 
