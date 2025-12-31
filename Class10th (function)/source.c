5c8884321649d85081cf523030d9f8710be1d340
coool8192-arch <coool8192@gmail.com>
2025-12-31 21:43:32 +0900

    251231 메모리 영역

---
+++ Class10th (function)/source.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void initialize()
+{
+	printf("initialize\n");
+}
+
+void coordinate(int x, int y)
+{
+	printf("x : %d\ny : %d\n", x, y);
+}
+
+void reset(char grade, int level, float experience)
+{
+	printf("grade : %c\nlevel : %d\nexp : %f\n", grade, level, experience);
+}
+
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int absolute(int x)
+{
+	//if (x >= 0)
+	//{
+	//	return x;
+	//}
+	//else
+	//{
+	//	return -x;
+	//}
+	(x > 0) ? x : -x;
+}
+
+void function(int x)
+{
+	//printf("%d\n", x);
+	//x--;
+	//if (x > 0)
+	//{
+	//	function(x);
+	//}
+	if (x <= 0)
+	{
+		return;
+	}
+	printf("%d\n", x);
+
+	function(x - 1);
+}
+
+inline int square(int x)
+{
+	return x * x;
+}
+
+double average(int count, ...)
+{
+	// va_list : 가변인수의 메모리 주소를 저장하는 포인터 변수입니다.
+
+	va_list pointer;
+
+	// va_start : 가변인수를 가져올 수 있도록 포인터를 설정하는 함수입니다.
+
+	va_start(pointer, count);
+
+	// va_arg : 가변인수 포인터에서 특정 자료형의 크기만큼 값을 가져오는 함수입니다.
+
+	double sum = 0;
+
+	for (int i = 0; i < count; i++)
+	{
+		sum += va_arg(pointer, int);
+	}
+
+	return sum / count;
+	// va_end : 가변인수가 끝났을 때 포인터를 NULL로 초기화하는 함수입니다.
+
+	va_end(count);
+}
+void main()
+{
+#pragma region 함수
+	// 하나의 특별한 목적의 작업을 수행하기 위해
+	// 독립적으로 설계된 코드의 집합입니다.
+
+	//initialize();
+
+#pragma endregion
+
+#pragma region 매개변수
+	// 함수의 정의에서 전달받은 인수를 함수 내부로
+	// 전달하기 위해 사용하는 변수입니다.
+
+	//coordinate(15, 24);
+
+	// 매개변수는 함수 내부에서만 연산이 이루어지며,
+	// 함수가 종료되면 메모리에서 해제됩니다.
+
+	// 하나의 함수에 서로 다른 자료형의 매개변수를 함께
+	// 정의할 수 있으며, 여러 개의 매개변수를 생성할 수 있습니다.
+
+	//reset('A', 30, 12.25f);
+#pragma endregion
+
+#pragma region 인수
+	// 함수가 호출될 때 매개변수에 실제로 전달되는 값입니다.
+
+	// 인수의 경우 함수에 있는 매개변수에 따라 전달할 수 있는
+	// 인수의 수가 결정되며, 값을 전달하는 인수와
+	// 전달받는 매개변수의 자료형이 일치해야 합니다.
+
+	//int m = 10;
+	//int n = 20;
+
+	//int u = m;
+	//
+	//m = n;
+	//
+	//n = u;
+
+	//printf("m : %d\nn : %d\n", m, n);
+	//
+	//swap(&m, &n);
+	//
+	//printf("m : %d\nn : %d\n", m, n);
+
+#pragma endregion
+
+#pragma region 반환형
+	// 함수가 실행을 마치고 호출한 쪽으로
+	// 어떤 자료형의 값을 반환할지 미리 알려주는 형식입니다.
+
+	//int x = 50;
+	//int y = -36;
+	//
+	//printf("x의 절댓값 : %d\n", absolute (x));
+	//printf("y의 절댓값 : %d\n", absolute(y));
+	//printf("절댓값 : %d\n", absolute (-5));
+
+	// 함수의 경우 자료형과 반환하는 값의 형태가
+	// 일치하지 않으면 원하는 값을 얻을 수 없습니다.
+
+#pragma endregion
+
+#pragma region 재귀함수
+	// 어떤 함수에서 자신을 다시 호출하여 작업을 수행하는 함수입니다.
+
+	//function(3);
+
+	// 재귀함수는 함수를 계속 호출하기 때문에 스택 영역에
+	// 메모리가 계속 쌓이게 되어 스택 오버플로우가 발생합니다.
+
+#pragma endregion
+
+#pragma region 인라인 함수
+	// 함수를 호출하는 대신 함수가 호출되는 위치마다
+	// 함수의 코드를 복사해 전달하는 함수입니다.
+
+	//printf("%d\n", square(3));
+
+	// 인라인 함수는 함수를 호출하는 과정이 없어 처리 속도가 빠르지만,
+	// 사용할 때마다 코드가 복사되어 파일의 크기가 커지게 됩니다.
+
+#pragma endregion
+
+#pragma region 가변인수 목록
+	// 고정된 매개변수 외에 개수가 정해지지 않은 인수를 추가로 받을 수 있는 인수입니다.
+
+	//printf("%f\n",average(3, 80, 90, 75));
+#pragma endregion
+
+}
--- Program/Source.c
+++ Program/Source.c
@@ -1,146 +1,75 @@
 #include <stdio.h>
 
-void initialize()
-{
-	printf("initialize\n");
-}
+int health = 5;
 
-void coordinate(int x, int y)
+void render()
 {
-	printf("x : %d\ny : %d\n", x, y);
-}
-
-void reset(char grade, int level, float experience)
-{
-	printf("grade : %c\nlevel : %d\nexp : %f\n", grade, level, experience);
-}
-
-void swap(int* a, int* b)
-{
-	int t = *a;
-	*a = *b;
-	*b = t;
-}
-
-int absolute(int x)
-{
-	//if (x >= 0)
-	//{
-	//	return x;
-	//}
-	//else
-	//{
-	//	return -x;
-	//}
-	(x > 0) ? x : -x;
-}
-
-void function(int x)
-{
-	//printf("%d\n", x);
-	//x--;
-	//if (x > 0)
-	//{
-	//	function(x);
-	//}
-	if (x <= 0)
+	if (health > 0)
 	{
-		return;
+		for (int i = 0; i < health; i++)
+		{
+			printf("♥ ");
+		}
 	}
-	printf("%d\n", x);
-
-	function(x - 1);
-}
-
-inline int square(int x)
-{
-	return x * x;
 }
 
 void main()
 {
-#pragma region 함수
-	// 하나의 특별한 목적의 작업을 수행하기 위해
-	// 독립적으로 설계된 코드의 집합입니다.
+#pragma region 메모리 영역
 
-	//initialize();
+#pragma region CODE 영역
+	// 프로그램을 실행하기 위해 필요한 코드가 저장되는 영역으로
+	// 함수의 주소와 상수가 저장되는 메모리 영역입니다.
+#pragma endregion
+
+#pragma region DATA 영역
+	// 프로그램의 시작과 함께 메모리에 저장되며
+	// 프로그램이 종료되면 메모리가 해제되는 영역입니다.
 
 #pragma endregion
 
-#pragma region 매개변수
-	// 함수의 정의에서 전달받은 인수를 함수 내부로
-	// 전달하기 위해 사용하는 변수입니다.
+#pragma region BSS 영역
+	// 프로그램이 실행될 때 초기화되지 않은 전역 변수와 정적 변수가 저장되는 메모리 영역입니다.
 
-	//coordinate(15, 24);
-
-	// 매개변수는 함수 내부에서만 연산이 이루어지며,
-	// 함수가 종료되면 메모리에서 해제됩니다.
-
-	// 하나의 함수에 서로 다른 자료형의 매개변수를 함께
-	// 정의할 수 있으며, 여러 개의 매개변수를 생성할 수 있습니다.
-
-	//reset('A', 30, 12.25f);
 #pragma endregion
 
-#pragma region 인수
-	// 함수가 호출될 때 매개변수에 실제로 전달되는 값입니다.
+#pragma region STACK 영역
+	// 프로그램이 자동으로 사용하는 임시 메모리 영역으로
+	// 함수 호출 시 생성되는 지역 변수와 매개 변수가 저장되는 메모리 영역입니다.
+#pragma endregion
 
-	// 인수의 경우 함수에 있는 매개변수에 따라 전달할 수 있는
-	// 인수의 수가 결정되며, 값을 전달하는 인수와
-	// 전달받는 매개변수의 자료형이 일치해야 합니다.
+#pragma region HEAP 영역
+	// 사용자가 직접 메모리 공간을 할당해주는 메모리 영역으로,
+	// 사용하지 않는 경우 사용자가 직접 메모리를 해제해야하는 메모리 영역입니다.
+#pragma endregion
 
-	//int m = 10;
-	//int n = 20;
+#pragma endregion
 
-	//int u = m;
+#pragma region 지역 변수
+	// 함수 내부에서 선언된 변수로 함수 내부에서만 접근할 수 있으며,
+	// 함수가 종료되면 메모리에서 사라지는 변수입니다.
+
+	//int x = 10;
 	//
-	//m = n;
+	//{
+	//	int x = 20;
 	//
-	//n = u;
-
-	//printf("m : %d\nn : %d\n", m, n);
+	//	printf("%d\n", x);
+	//}
 	//
-	//swap(&m, &n);
-	//
-	//printf("m : %d\nn : %d\n", m, n);
+	//printf("%d\n", x);
 
 #pragma endregion
 
-#pragma region 반환형
-	// 함수가 실행을 마치고 호출한 쪽으로
-	// 어떤 자료형의 값을 반환할지 미리 알려주는 형식입니다.
+#pragma region 전역 변수
+	// 함수 외부에서 선언된 변수로 프로그램이 실행될 때 메모리에 올라가고,
+	// 프로그램이 종료되면 메모리에서 해제되는 변수입니다.
 
-	//int x = 50;
-	//int y = -36;
-	//
-	//printf("x의 절댓값 : %d\n", absolute (x));
-	//printf("y의 절댓값 : %d\n", absolute(y));
-	//printf("절댓값 : %d\n", absolute (-5));
+	health -= 1;
 
-	// 함수의 경우 자료형과 반환하는 값의 형태가
-	// 일치하지 않으면 원하는 값을 얻을 수 없습니다.
+	render();
 
 #pragma endregion
 
-#pragma region 재귀함수
-	// 어떤 함수에서 자신을 다시 호출하여 작업을 수행하는 함수입니다.
-
-	//function(3);
-
-	// 재귀함수는 함수를 계속 호출하기 때문에 스택 영역에
-	// 메모리가 계속 쌓이게 되어 스택 오버플로우가 발생합니다.
-
-#pragma endregion
-
-#pragma region 인라인 함수
-	// 함수를 호출하는 대신 함수가 호출되는 위치마다
-	// 함수의 코드를 복사해 전달하는 함수입니다.
-
-	printf("%d\n", square(3));
-
-	// 인라인 함수는 함수를 호출하는 과정이 없어 처리 속도가 빠르지만,
-	// 사용할 때마다 코드가 복사되어 파일의 크기가 커지게 됩니다.
-
-#pragma endregion
 
 }
